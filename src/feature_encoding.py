ee271116dfb78a466479b991bc85a5069e02e1c0
Dean Hettiarachchi <123150002+deaneeth@users.noreply.github.com>
2025-09-15 19:55:15 +0530

    refresh

--- src/feature_encoding.py
+++ src/feature_encoding.py
@@ -1,3 +1,8 @@
+# This file is responsible for defining various feature encoding strategies for categorical variables.
+# It includes implementations for nominal and ordinal encoding.
+# The module is designed to be flexible, allowing for easy addition of new encoding strategies as needed
+# It integrates with the overall data processing pipeline to ensure that categorical features are appropriately transformed for model training.
+
 import logging
 import pandas as pd
 import os
@@ -9,12 +14,11 @@ logging.basicConfig(level=logging.INFO, format=
     '%(asctime)s - %(levelname)s - %(message)s')
 
 
-class FeatureEncodingStrategy(ABC):
+class FeatureEncodingStrategy(ABC):                # the class that inherited by 'Abstract base class' is the 'FeatureEncodingStrategy'
     @abstractmethod
-    def encode(self, df: pd.DataFrame) ->pd.DataFrame:
+    def encode(self, df: pd.DataFrame) ->pd.DataFrame:  
         pass
 
-
-class VariableType(str, Enum):
+class VariableType(str, Enum):     # Define variable types for encoding strategies
     NOMINAL = 'nominal'
     ORDINAL = 'ordinal'
